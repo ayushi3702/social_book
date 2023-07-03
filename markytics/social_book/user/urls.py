26c88caafcd2529f533f4faf3b78b175ef85b656
Ayushi Prasad <ayushipd02@gmail.com>
2023-07-03 18:59:53 +0530

    commit

---
+++ markytics/social_book/user/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register/', views.registeruser, name='register'),
+    path('login/', views.loginuser, name='login'),
+    path('logout/', views.logoutuser, name='logout'),
+]
